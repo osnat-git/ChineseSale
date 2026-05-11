1ed503e03dc4ba8905cffe0afb061b627dbfdee1
osnat-git <39215866328@mby.co.il>
2026-05-11 18:16:12 +0300

    עדכון קובץ gitignore

--- backend/ApiProject/Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ backend/ApiProject/Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56528bcdc34ca148451fef36a93c90163879c9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee96c5d53b6a2a1ef501c69b414d44243acee211")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
